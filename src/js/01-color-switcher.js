67f219aa4a6d2fadb65d4f8596e249e9c1c5714e
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-10-09 22:12:04 +0200

    the first task is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const startBtnHandler = () => {
+  startBtn.setAttribute('disabled', true);
+
+  if (intervalId) {
+    return;
+  }
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stoptBtnHandler = () => {
+  clearInterval(intervalId);
+
+  intervalId = null;
+
+  startBtn.removeAttribute('disabled');
+};
+
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stoptBtnHandler);
