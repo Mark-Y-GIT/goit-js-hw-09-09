1687743112f47cbc199c79778c2b87cb7970f62a
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-11-01 15:26:23 +0100

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,17 @@
-const form = document.querySelector('.form');
-let formData = null;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function convertToNumber({ delay, step, amount }) {
-  formData = {
-    delay: Number(delay),
-    step: Number(step),
-    amount: Number(amount),
-  };
+const form = document.querySelector('.form');
+let delay = null;
+let step = null;
+let amount = null;
+let promiseNumber = 1;
+
+function convertToNumber(data) {
+  const { delay: delayString, step: stepString, amount: amountString } = data;
+
+  delay = Number(delayString);
+  step = Number(stepString);
+  amount = Number(amountString);
 }
 
 function getFormData(e) {
@@ -19,11 +24,46 @@ function getFormData(e) {
   return data;
 }
 
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  promiseNumber += 1;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function runPromises() {
+  for (let i = 0; i < amount; i += 1) {
+    if (promiseNumber !== 1) {
+      delay = delay + step;
+    }
+
+    createPromise(promiseNumber, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const data = getFormData(e);
 
   convertToNumber(data);
-  console.log(formData);
+
+  runPromises();
+
+  promiseNumber = 1;
 });
